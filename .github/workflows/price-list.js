bc517101dda21500f6ff3f899711378287c3b9b6
Tuomo Helo <tuomo.helo@turkuamk.fi>
2023-03-08 10:58:19 +0200

    Add price-list.js

---
+++ .github/workflows/price-list.js
@@ -0,0 +1,17 @@
+
+let aPriceList;
+
+export const setPriceList = (priceList) => {
+  aPriceList = priceList;
+};
+
+export const getPrice = (itemId, quantity) => {
+  return 188.75;
+};
+
+export const priceList = {
+  setPriceList,
+  getPrice
+};
+
+//export default const...
